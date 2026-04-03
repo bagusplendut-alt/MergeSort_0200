a3aabdc802c72dd3209ff047d3067013899e37cd
bagusplendut-alt <bagusplendut@gmail.com>
2026-04-03 14:55:14 +0700

    mengvalidasi input dan mengambil input jumlah elemen

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -5,3 +5,8 @@ using namespace std;
 int arr[20], B[20];
 // n adalah ukuran input array
 int n;
+
+void input() {
+    while (true) {
+        cout << "Masukkan panjang element array : ";
+        cin >> n;
