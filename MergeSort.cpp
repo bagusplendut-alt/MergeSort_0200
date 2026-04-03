c96279ed5b90066cec6c59fca8648fb3904eff09
bagusplendut-alt <bagusplendut@gmail.com>
2026-04-03 14:58:29 +0700

    menambahkan validasi input

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -10,3 +10,10 @@ void input() {
     while (true) {
         cout << "Masukkan panjang element array : ";
         cin >> n;
+
+        if (n <= 20) {
+            break;
+        } else {
+            cout << "\nMaksimal panjang array adalah 20" << endl;
+        }
+    }
